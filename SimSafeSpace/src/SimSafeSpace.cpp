0014500a4b9bb5a5cd71846f23c92217b388d5fe
shbae27 <shabae27@hhi.co.kr>
2016-06-16 19:00:18 +0900

    2016/06/16 5ms 주기로 송/수신 기능 추가

--- SimSafeSpace/src/SimSafeSpace.cpp
+++ SimSafeSpace/src/SimSafeSpace.cpp
@@ -20,7 +20,12 @@ using namespace std;
 //using FUNC_TYPE = int(const std::string&);
 
 static std::mutex g_mutex_Printout;
+static std::mutex g_mutex_UserTerminate;
+static bool g_bTerminate = false;
+
 const static std::string strMainBdIpAddr("192.168.1.128");
+
+std::condition_variable g_condition_UserTerminate;
 
 void ThreadFunc(int& Aug)
 {
@@ -33,38 +38,34 @@ void ThreadFunc(int& Aug)
 	while (10 >= Aug);
 }
 
-int ThreadGetUserInput(const std::string& strAug)
+int ThreadCount(const std::string& strAug)
 {
 	std::cout << strAug.c_str() << std::endl;
-	std::this_thread::sleep_for(std::chrono::seconds(1));
-	std::cout << "starting getting the input from user..." << std::endl;
+	uint32_t nLoopCount = 0;
 
-	std::string strInput;
-	int nValue = 0;
-	do
+	while(true)
 	{
-		std::getline(cin, strInput);
-		nValue = strInput.compare("end");
-		std::cout << "echo: " << strInput.c_str() << std::endl;
-		std::cout << "compare result: " << nValue << std::endl;
-	} while (0 != nValue);
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		nLoopCount++;
+		cout<<"[ThreadCount] loop count :"<<nLoopCount<<endl;
+		if(10 == nLoopCount)
+		{
+			cout<<"[ThreadCount] this loop ends"<<endl;
+			break;
+		}
+	}
+
+	g_bTerminate = true;
+	g_condition_UserTerminate.notify_all();
 
 	return 1;
 }
 
 int ThreadSimSafeSpace()
 {
-	WSADATA wsaData;
-
-	if(0 != WSAStartup(MAKEWORD(2,2), &wsaData))
-	{
-		int nRetValue = 0;
-		nRetValue = ::WSAGetLastError();
-		return nRetValue;
-	}
 
 	int nListenChanl = 0;
-	sockaddr_in AddrClient;
+	sockaddr_in AddrClient, AddrMainBd;
 
 	nListenChanl = ::socket(PF_INET, SOCK_DGRAM, 0);
 	if(INVALID_SOCKET == nListenChanl)
@@ -77,6 +78,10 @@ int ThreadSimSafeSpace()
 	AddrClient.sin_family = AF_INET;
 	AddrClient.sin_addr.S_un.S_addr = inet_addr("192.168.1.48");
 	AddrClient.sin_port = htons(5001);
+
+	AddrMainBd.sin_family = AF_INET;
+	AddrMainBd.sin_addr.S_un.S_addr = inet_addr("192.168.1.128");
+	AddrMainBd.sin_port = htons(5001);
 
 	if(SOCKET_ERROR == ::bind(nListenChanl, (sockaddr*)&AddrClient, sizeof(sockaddr_in)))
 	{
@@ -98,17 +103,24 @@ int ThreadSimSafeSpace()
 	
 	unsigned int nLoopCount = 0;
 
+	uint32_t nPrevCounter = 0;
 	while(TRUE)
 	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+		
+		nLoopCount++;
 		
 		DataToSend.StartFrame = htonl(STARTFRAME);
 		DataToSend.EndFrame = htonl(ENDFRAME);
 		DataToSend.nCounter = htonl(nLoopCount);
 
 		// Send test-data to main B/D
-
-
+		int nSendByte = ::sendto(nListenChanl, reinterpret_cast<char*>(&DataToSend), sizeof(CommTestFrame), 0, reinterpret_cast<struct sockaddr*>(&AddrMainBd), sizeof(struct sockaddr));
+		if(SOCKET_ERROR == nSendByte)
+		{
+			cout<<"[ThreadSimSafeSpace] fail to sendto"<<endl;
+		}
+		
 		sTimeout.tv_sec = 0;
 		sTimeout.tv_usec = 0;
 		tempFdList = readFd;
@@ -123,12 +135,21 @@ int ThreadSimSafeSpace()
 					{
 						sockaddr_in AddrSender;
 						int nSockAddrSize = sizeof(sockaddr_in);
-						::recvfrom(nListenChanl, RecvBuffer.get(), 1024, 0, (struct sockaddr*)&AddrSender, &nSockAddrSize);
-					
+						int nRecvByte = ::recvfrom(nListenChanl, RecvBuffer.get(), 1024, 0, (struct sockaddr*)&AddrSender, &nSockAddrSize);	
+						//cout<<"[ThreadSimSafeSpace] RecvByte: "<<nRecvByte<<endl;
+
+						CommTestFrame RecvDataFrame;
+						memcpy(reinterpret_cast<char*>(&RecvDataFrame), RecvBuffer.get(), nRecvByte);
+						uint32_t nCurrCounter = ntohl(RecvDataFrame.nCounter);
+						if(nCurrCounter != nPrevCounter + 1)
+						{
+							cout<<"[ThreadSimSafeSpace] Counter didn't match, CurrCounter: "<<nCurrCounter<<", PrevCounter: "<<nPrevCounter<<endl;
+						}
+
+						nPrevCounter = nCurrCounter;
 					}
 				}
 			}
-
 		}
 		else if(0 == nRetValue)
 		{
@@ -139,27 +160,51 @@ int ThreadSimSafeSpace()
 			;
 		}
 
+		{
+			std::unique_lock<std::mutex> lock(g_mutex_UserTerminate);
+			bool bRetValue = g_condition_UserTerminate.wait_for(lock, std::chrono::microseconds(0), [](){ return (true==g_bTerminate);});
+			//bool bRetValue = g_bTerminate;
+			if(true == bRetValue)
+			{
+				cout<<"[ThreadSimSafeSpace] this loop ends"<<endl;
+				break;
+			}
+		}
 
 	}
-
 	closesocket(nListenChanl);
-	WSACleanup();
 
 	return 0;
 }
 
 int main()
 {
-	int a = 0;
-	//std::thread ThreadInstance(ThreadFunc, std::ref(a));
-	std::string strMessage("thread is running");
-	std::packaged_task<int(const std::string&)> task(ThreadGetUserInput);
-	std::future<int> RetValue = task.get_future();
+	WSADATA wsaData;
 
-	std::thread ThreadInstance2(std::move(task), std::ref(strMessage));
-	ThreadInstance2.detach();
-	int nValue = RetValue.get();
+	if(0 != WSAStartup(MAKEWORD(2,2), &wsaData))
+	{
+		int nRetValue = 0;
+		nRetValue = ::WSAGetLastError();
+		return nRetValue;
+	}
+
+	std::packaged_task<int()> TaskInterface(ThreadSimSafeSpace);
+	std::future<int> futureThreadInterface = TaskInterface.get_future();
+	std::thread ThreadInterface(std::move(TaskInterface));
+	ThreadInterface.detach();
+	
+	std::string strMessage("ThreadCount is running");
+	std::packaged_task<int(const std::string&)> TaskCount(ThreadCount);
+	std::future<int> futureTaskCount = TaskCount.get_future();
+	std::thread ThreadCount(std::move(TaskCount),  std::ref(strMessage));
+	ThreadCount.detach();
+
+	int nValue = 0;
+	nValue = futureTaskCount.get();
+	nValue = futureThreadInterface.get();
+
 	std::cout << "[main] terminated" << std::endl;
+	WSACleanup();
 
     return 0;
 }
